98f77c2ff10637bfb076fcd2232443779bd3bae2
QC-Scooter <gavinmacalpine@gmail.com>
2022-03-30 21:36:38 -0500

    Added folders for organizing

---
+++ code/main.py
@@ -0,0 +1,57 @@
+import pygame
+import math
+import random
+import sys
+import stickman_library
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
+PI = math.pi
+
+
+screenSize = (1600, 900)
+
+pygame.init()
+
+
+screen = pygame.display.set_mode(screenSize, pygame.RESIZABLE)
+
+pygame.display.set_caption("Stickmen v0.1 ALPHA")
+
+done = False
+
+clock = pygame.time.Clock()
+
+clockSpeed = 60
+
+
+class StickmanFighter(object):
+    def __init__(self, data):
+        stickman_color = data
+        print("Stickman initiated")
+        print(stickman_color)
+
+#    def stickSpawn(self):
+#
+
+
+class screenBackground(object):
+    def __init__(self, data):
+        backgroundImage = data
+        print("Background initiated")
+
+
+red_stickman = StickmanFighter(RED)
+blue_stickman = StickmanFighter(BLUE)
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+            pygame.quit()
+            sys.exit()
+            break
