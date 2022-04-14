9c32967f4b2a8f5db45dc5c779c8fd9219782019
QC-Scooter <gavinmacalpine@gmail.com>
2022-04-14 13:06:27 -0500

    changed background color

--- libraries/backgrounds.py
+++ libraries/backgrounds.py
@@ -5,7 +5,10 @@ screenSize = (1600, 900)
 screen = pygame.display.set_mode(screenSize, pygame.RESIZABLE)
 
 def forest():
-	screen.fill((0, 204, 255))
+	screen.fill((23, 195, 255))
+	pygame.draw.rect(screen, (0, 153, 51), (0, 700, 1600, 200))
+	pygame.draw.rect(screen, (112, 51, 8), (50, 350, 75, 350))
+	pygame.draw.rect(screen, (112, 51, 8), (1475, 350, 75, 350))
 
 def white():
 	screen.fill((255, 255, 255))
